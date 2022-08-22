8235003808fd2a5c0aa17bc46fc5f3d7ed340ce9
WithLithum <WithLithum@foxmail.com>
2022-08-22 15:00:20 +0800

    ✨ 史莱姆生成禁止 (closes #I5NFHY)

---
+++ src/main/java/io/github/nexuskrop/stackpun/worlds/GameEventsHandler.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2022.
+ * This file is part of StackPun.
+ * Licensed under GNU AGPL version 3 or later.
+ */
+
+package io.github.nexuskrop.stackpun.worlds;
+
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
+
+public class GameEventsHandler implements Listener {
+    @EventHandler
+    public void onEntitySpawn(EntitySpawnEvent event) {
+        // TODO 可配置
+        if (event.getEntityType() == EntityType.SLIME) {
+            event.setCancelled(true);
+        }
+    }
+}
