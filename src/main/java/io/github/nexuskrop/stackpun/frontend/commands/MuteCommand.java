8c5c7f0e85c38e3c65a6ed2a0a3217edce9cc2d9
WithLithum <WithLithum@foxmail.com>
2022-08-22 08:43:06 +0800

    ✨ 禁言命令
    - mute 命令
    - unmuted 命令

---
+++ src/main/java/io/github/nexuskrop/stackpun/frontend/commands/MuteCommand.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2022.
+ * This file is part of StackPun.
+ * Licensed under GNU AGPL version 3 or later.
+ */
+
+package io.github.nexuskrop.stackpun.frontend.commands;
+
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.CommandPermission;
+import dev.jorel.commandapi.arguments.PlayerArgument;
+import io.github.nexuskrop.stackpun.StackPun;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class MuteCommand implements StackCommand {
+
+    @Override
+    public void register() {
+        new CommandAPICommand("mute")
+                .withPermission(CommandPermission.OP)
+                .withArguments(new PlayerArgument("target"))
+                .executesConsole(this::execute)
+                .executesPlayer(this::execute)
+                .register();
+    }
+
+    public void execute(CommandSender sender, Object[] args) {
+        var player = (Player) args[0];
+        var profile = StackPun.instance().profileManager().getProfile(player);
+
+        if (profile.muted) {
+            sender.sendMessage(Component.text("The user is already muted!").color(NamedTextColor.RED));
+        } else {
+            profile.muted = true;
+            StackPun.instance().profileManager().putProfile(player, profile);
+        }
+    }
+}
---
+++ src/main/java/io/github/nexuskrop/stackpun/frontend/commands/UnmuteCommand.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2022.
+ * This file is part of StackPun.
+ * Licensed under GNU AGPL version 3 or later.
+ */
+
+package io.github.nexuskrop.stackpun.frontend.commands;
+
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.CommandPermission;
+import dev.jorel.commandapi.arguments.PlayerArgument;
+import io.github.nexuskrop.stackpun.StackPun;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class UnmuteCommand implements StackCommand {
+    @Override
+    public void register() {
+        new CommandAPICommand("unmute")
+                .withPermission(CommandPermission.OP)
+                .withArguments(new PlayerArgument("target"))
+                .executesConsole(this::execute)
+                .executesPlayer(this::execute)
+                .register();
+    }
+
+    public void execute(CommandSender sender, Object[] args) {
+        var player = (Player) args[0];
+        var profile = StackPun.instance().profileManager().getProfile(player);
+
+        if (!profile.muted) {
+            sender.sendMessage(Component.text("The user was not muted!").color(NamedTextColor.RED));
+        } else {
+            profile.muted = false;
+            StackPun.instance().profileManager().putProfile(player, profile);
+        }
+    }
+}
