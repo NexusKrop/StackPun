f1a9345d2bf367abcac4702655514487c659d303
WithLithum <WithLithum@foxmail.com>
2022-08-26 08:52:51 +0800

    ✨ STAP-6 完成
    更多信息以后会加入

--- src/main/java/io/github/nexuskrop/stackpun/frontend/commands/WhoAmICommand.java
+++ src/main/java/io/github/nexuskrop/stackpun/frontend/commands/WhoAmICommand.java
@@ -7,11 +7,13 @@
 package io.github.nexuskrop.stackpun.frontend.commands;
 
 import dev.jorel.commandapi.CommandAPICommand;
+import io.github.nexuskrop.stackpun.util.Common;
 
 public class WhoAmICommand implements StackCommand {
     private static final String LINE_1 = "commands.whoami.line1";
     private static final String LINE_2 = "commands.whoami.line2";
     private static final String LINE_3 = "commands.whoami.line3";
+    private static final String LINE_4 = "commands.whoami.line4";
 
     @Override
     public void register() {
@@ -20,8 +22,9 @@ public class WhoAmICommand implements StackCommand {
                 .withFullDescription("See your information")
                 .executesPlayer((sender, args) -> {
                     StackCommand.sendMessageLoc(sender, LINE_1);
-                    StackCommand.sendMessageVal(sender, LINE_2, sender);
-                    StackCommand.sendMessageVal(sender, LINE_2, StackCommand.getMuted(sender));
+                    StackCommand.sendMessageVal(sender, LINE_2, Common.getEntityRepresent(sender));
+                    StackCommand.sendMessageVal(sender, LINE_3, StackCommand.getMuted(sender));
+                    StackCommand.sendMessageVal(sender, LINE_4, sender.getClientBrandName());
                 }).register();
     }
 }
